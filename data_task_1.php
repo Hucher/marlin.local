d1085de5253a9ac0709527829a3c14fabd03d9c7
Hucher <maksim.shishkin.2013@mail.ru>
2020-11-02 00:46:14 +0300

    completed 1 task

---
+++ data_task_1.php
@@ -0,0 +1,22 @@
+<?php
+$filterList = [
+    [
+        'title' => 'Reports',
+        'tag' => 'reports file'
+    ],
+
+    [
+        'title' => 'Analytics',
+        'tag' => 'analytics graphs'
+    ],
+
+    [
+        'title' => 'Export',
+        'tag' => 'export download'
+    ],
+
+    [
+        'title' => 'Storage',
+        'tag' => 'Storage'
+    ]
+];
